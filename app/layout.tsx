2d93eb664fed93769591be051b703124711d84fa
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-09-21 15:10:12 +0800

    feat: add poppins sans font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,17 @@
 import type { Metadata } from "next";
 import { headers } from "next/headers";
 
-import { Londrina_Solid } from "next/font/google";
+import { Londrina_Solid, Poppins } from "next/font/google";
 import "./globals.css";
 
 import Provider from "./provider";
+
+const poppins = Poppins({
+  variable: "--font-poppins",
+  subsets: ["latin"],
+  display: "swap",
+  weight: ["400", "500", "600", "700"],
+});
 
 const londrinaSolid = Londrina_Solid({
   variable: "--font-londrina-solid",
@@ -15,7 +22,8 @@ const londrinaSolid = Londrina_Solid({
 
 export const metadata: Metadata = {
   title: "Geeg - A Talent Finder Platform",
-  description: "Geeg is a talent finder platform to make it easy to you finished your business.",
+  description:
+    "Geeg is a talent finder platform to make it easy to you finished your business.",
 };
 
 export default function RootLayout({
@@ -27,7 +35,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <Provider cookies={cookies}>
-        <body className={`${londrinaSolid.variable} antialiased`}>
+        <body className={`${poppins.variable} ${londrinaSolid.variable} antialiased`}>
           {children}
         </body>
       </Provider>
