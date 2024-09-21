76237b644081708b8d482aa672d96b2402c56d1e
Marcellino Chris O'Vara <marcellinocovara@gmail.com>
2024-09-21 10:43:44 +0800

    feat: install reown appkit

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,8 @@ import Image from "next/image";
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 
+import { SearchIcon } from "lucide-react";
+
 export default function Home() {
   return (
     <div className="min-h-screen bg-white">
@@ -17,7 +19,8 @@ export default function Home() {
           </Link>
 
           <nav className="hidden space-x-4 md:flex">
-            <Button>Join</Button>
+            <w3m-button />
+            {/* <Button>Join</Button> */}
           </nav>
         </div>
       </header>
@@ -65,25 +68,5 @@ export default function Home() {
         </div>
       </section>
     </div>
-  );
-}
-
-function SearchIcon(props) {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <circle cx="11" cy="11" r="8" />
-      <path d="m21 21-4.3-4.3" />
-    </svg>
   );
 }
